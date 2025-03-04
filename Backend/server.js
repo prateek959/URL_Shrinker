ebbcc1d00f77a293790adc7bfa9521747fd28953
prateek959 <prateekkush8081@gmail.com>
2025-03-04 15:13:02 +0530

    add

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ import analyticsRouter from "./routes/analytics.routes.js";
 const app = express();
 const PORT = process.env.PORT;
 
-const frontendUrl = "https://url-shrinker-25.netlify.app/";
+const frontendUrl = "https://url-shrinker-25.netlify.app";
 
 const corsOptions = {
   origin: frontendUrl,
